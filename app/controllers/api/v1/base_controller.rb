6ed8849d597fba30e81fa4be5d8ba51cb4cebe5c
Cassius Chen <chzsh1995@gmail.com>
2014-04-02 10:01:51 +0000

    clean up

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,3 +1,8 @@
 class Api::V1::BaseController < ApplicationController
   respond_to :json
+
+  private
+    def current_resource_owner
+      User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
+    end
 end
--- app/controllers/api/v1/omniauth_controller.rb
+++ app/controllers/api/v1/omniauth_controller.rb
@@ -1,13 +1,7 @@
 class Api::V1::OmniauthController < Api::V1::BaseController
-
   doorkeeper_for :all
 
   def index
-    respond_with current_token_owner
-  end
-
-  private
-  def current_token_owner
-    User.find(doorkeeper_token.resource_owner_id) if doorkeeper.token
+    respond_with current_resource_owner
   end
 end
