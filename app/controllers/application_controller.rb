57a1a36eb4da730ad4a7cc0bba3e56811f84c48d
Cassius Chen <chzsh1995@gmail.com>
2014-04-04 01:13:41 +0800

    allow using either pku_id & email to login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,5 +8,7 @@ class ApplicationController < ActionController::Base
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << [:name, :pku_id]
+    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :pku_id, :email, :password, :remember_me) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:pku_id, :email, :password, :password_confirmation, :current_password) }
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,18 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :lockable, :timeoutable,:omniauthable
+  validates :pku_id, :uniqueness => {
+    :case_sensitive => false
+  }
 
+  attr_accessor :login
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(pku_id) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions).first
+    end
+  end
 end
