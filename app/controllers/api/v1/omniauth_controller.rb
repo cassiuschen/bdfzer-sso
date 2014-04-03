082bff9c94c6095ec96dd41cada100e999d95b67
Cassius Chen <chzsh1995@gmail.com>
2014-04-03 08:13:13 +0800

    fix the respond json

--- app/controllers/api/v1/omniauth_controller.rb
+++ app/controllers/api/v1/omniauth_controller.rb
@@ -2,6 +2,18 @@ class Api::V1::OmniauthController < Api::V1::BaseController
   doorkeeper_for :all
 
   def index
-    respond_with current_resource_owner
+    hash = {
+      provider: 'bdfzer',
+      id: current_resource_owner.id.to_s,
+      info: {
+        email: current_resource_owner.email
+      },
+      extra: {
+        name: current_resource_owner.name,
+        pku_id: current_resource_owner.pku_id
+      }
+    }
+
+    render json: hash.to_json
   end
 end
