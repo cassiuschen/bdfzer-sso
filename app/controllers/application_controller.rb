d589b4b8e2369df91d98db7c740285ed2c222e8a
Cassius Chen <chzsh1995@gmail.com>
2014-04-04 00:43:59 +0800

    finish build up sign-in and sign-up form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   #protect_from_forgery with: :exception
   protect_from_forgery with: :null_session
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << [:name, :pku_id]
+  end
 end
