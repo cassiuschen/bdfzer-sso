96a0062090df673c3d26c3966d6cbd432ec0726b
Cassius Chen <chzsh1995@gmail.com>
2014-04-25 13:02:16 +0800

    resize avatar to 240*240

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -30,7 +30,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
- # process :scale => [200, 300]
+process :resize_to_fill => [240, 240]
 #  #
 #  def scale(width, height)
 #  #   # do something
