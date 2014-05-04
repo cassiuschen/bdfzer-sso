d7d4fbb9489f66768b96e71882b967ef743817d2
Cassius Chen <chzsh1995@gmail.com>
2014-05-04 18:21:35 +0800

    production env.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,20 +30,6 @@ class ApplicationController < ActionController::Base
       'layouts/application'
     end
   end
-<<<<<<< HEAD
-=======
-
-  def store_location
-    # store last url - this is needed for post-login redirect to whatever the user last visited.
-    if (!request.xhr?) # don't store ajax calls
-      session[:previous_url] = request.fullpath
-    end
-  end
-
-  def after_sign_in_path_for(resource)
-    session[:previous_url] || root_path
-  end
->>>>>>> 77c680e85c1d27dc729eab856c39d0ab6acd0314
 end
 #request.fullpath != "/users/sign_in" &&
 #        request.fullpath != "/users/sign_up" &&
